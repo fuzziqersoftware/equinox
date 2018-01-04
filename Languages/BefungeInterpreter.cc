23c09210b4d16b0abc33c2febeedcb17df7c30b1
Martin Michelsen <mjem@wildblue.net>
2018-01-03 22:32:21 -0800

    clean up code a bit

--- Languages/BefungeInterpreter.cc
+++ Languages/BefungeInterpreter.cc
@@ -60,230 +60,230 @@ void befunge_interpret(const string& filename, uint8_t dimensions,
     } catch (const out_of_range&) { }
 
     switch (opcode) {
-        case -1:
-          // boundary cell. figure out what's missing and fix it
-          throw invalid_argument(string_printf("boundary cell %zd %zd", pos.x, pos.y));
-          break;
+      case -1:
+        // boundary cell. figure out what's missing and fix it
+        throw invalid_argument(string_printf("boundary cell %zd %zd", pos.x, pos.y));
+        break;
 
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-          stack.push(opcode - '0');
-          break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        stack.push(opcode - '0');
+        break;
 
-        case 'a':
-        case 'b':
-        case 'c':
-        case 'd':
-        case 'e':
-        case 'f':
-          stack.push(opcode - 'a' + 10);
-          break;
+      case 'a':
+      case 'b':
+      case 'c':
+      case 'd':
+      case 'e':
+      case 'f':
+        stack.push(opcode - 'a' + 10);
+        break;
 
-        case '+':
-          stack.push(stack.pop() + stack.pop());
-          break;
-        case '-': {
-          int64_t a = stack.pop();
-          int64_t b = stack.pop();
-          stack.push(b - a);
-          break;
+      case '+':
+        stack.push(stack.pop() + stack.pop());
+        break;
+      case '-': {
+        int64_t a = stack.pop();
+        int64_t b = stack.pop();
+        stack.push(b - a);
+        break;
+      }
+      case '*':
+        stack.push(stack.pop() * stack.pop());
+        break;
+      case '/': {
+        int64_t a = stack.pop();
+        int64_t b = stack.pop();
+        stack.push(b / a);
+        break;
+      }
+      case '%': {
+        int64_t a = stack.pop();
+        int64_t b = stack.pop();
+        stack.push(b % a);
+        break;
+      }
+
+      case '!': // logical not
+        stack.push(!stack.pop());
+        break;
+
+      case '`': {
+        int64_t a = stack.pop();
+        int64_t b = stack.pop();
+        stack.push(b > a);
+        break;
+      }
+
+      case '<': // move left
+        pos.face(-1, 0, 0);
+        break;
+      case '>': // move right
+        pos.face(1, 0, 0);
+        break;
+      case '^': // move up
+        pos.face(0, -1, 0);
+        break;
+      case 'v': // move down
+        pos.face(0, 1, 0);
+        break;
+      case '[': // turn left
+        pos.turn_left();
+        break;
+      case ']': // turn right
+        pos.turn_right();
+        break;
+
+      case '?': // move randomly
+        switch (rand() & 3) {
+          case 0:
+            pos.face(-1, 0, 0);
+            break;
+          case 1:
+            pos.face(1, 0, 0);
+            break;
+          case 2:
+            pos.face(0, -1, 0);
+            break;
+          case 3:
+            pos.face(0, 1, 0);
+            break;
         }
-        case '*':
-          stack.push(stack.pop() * stack.pop());
-          break;
-        case '/': {
-          int64_t a = stack.pop();
-          int64_t b = stack.pop();
-          stack.push(b / a);
-          break;
-        }
-        case '%': {
-          int64_t a = stack.pop();
-          int64_t b = stack.pop();
-          stack.push(b % a);
-          break;
-        }
+        break;
 
-        case '!': // logical not
-          stack.push(!stack.pop());
-          break;
+      case '_': // right if zero, left if not
+        pos.face(stack.pop() ? -1 : 1, 0, 0);
+        break;
 
-        case '`': {
-          int64_t a = stack.pop();
-          int64_t b = stack.pop();
-          stack.push(b > a);
-          break;
-        }
+      case '|': // down if zero, up if not
+        pos.face(0, stack.pop() ? -1 : 1, 0);
+        break;
 
-        case '<': // move left
-          pos.face(-1, 0, 0);
-          break;
-        case '>': // move right
-          pos.face(1, 0, 0);
-          break;
-        case '^': // move up
-          pos.face(0, -1, 0);
-          break;
-        case 'v': // move down
-          pos.face(0, 1, 0);
-          break;
-        case '[': // turn left
-          pos.turn_left();
-          break;
-        case ']': // turn right
-          pos.turn_right();
-          break;
-
-        case '?': // move randomly
-          switch (rand() & 3) {
-            case 0:
-              pos.face(-1, 0, 0);
-              break;
-            case 1:
-              pos.face(1, 0, 0);
-              break;
-            case 2:
-              pos.face(0, -1, 0);
-              break;
-            case 3:
-              pos.face(0, 1, 0);
-              break;
-          }
-          break;
-
-        case '_': // right if zero, left if not
-          pos.face(stack.pop() ? -1 : 1, 0, 0);
-          break;
-
-        case '|': // down if zero, up if not
-          pos.face(0, stack.pop() ? -1 : 1, 0);
-          break;
-
-        case '\"': { // push an entire string
-          pos.move_forward();
-          for (;;) {
-            int16_t value = field.get(pos.x, pos.y, pos.z);
-            if (value == '\"') {
-              break;
-            }
-
-            stack.push(value);
-            pos.move_forward();
-          }
-
-          // pos now points to the terminal quote; we'll automatically move
-          // forward after this loop
-          break;
-        }
-
-        case ';': { // skip over a segment
-          pos.move_forward();
-          for (;;) {
-            int16_t value = field.get(pos.x, pos.y, pos.z);
-            if (value == ';') {
-              break;
-            }
-            pos.move_forward();
-          }
-
-          // pos now points to the terminal semicolon; we'll automatically move
-          // forward after this loop
-          break;
-        }
-
-        case ':': { // duplicate top of stack
-          int64_t i = stack.pop();
-          stack.push(i);
-          stack.push(i);
-          break;
-        }
-
-        case '\\': { // swap top 2 items on stack
-          int64_t a = stack.pop();
-          int64_t b = stack.pop();
-          stack.push(a);
-          stack.push(b);
-          break;
-        }
-
-        case '$': // discard top of stack
-          stack.pop();
-          break;
-
-        case '.': // pop and print as integer followed by space
-          printf("%" PRId64 " ", stack.pop());
-          break;
-
-        case ',': // pop and print as ascii character
-          putchar(stack.pop());
-          break;
-
-        case ' ': // skip this cell
-          break;
-
-        case '#': // skip this cell and next cell
-          pos.move_forward();
-          break;
-
-        case 'p': { // write program space
-          int64_t y = stack.pop();
-          int64_t x = stack.pop();
-          int64_t v = stack.pop();
-          field.set(x, y, 0, v);
-          break;
-        }
-
-        case 'g': // read program space
-          try {
-            int64_t y = stack.pop();
-            int64_t x = stack.pop();
-            stack.push(field.get(x, y, 0));
-          } catch (const out_of_range&) {
-            stack.push(' ');
-          }
-          break;
-
-        case '&': { // push user-supplied number
-          int64_t i;
-          scanf("%" PRId64, &i);
-          stack.push(i);
-          break;
-        }
-
-        case '~': // push user-supplied character
-          stack.push(getchar());
-          break;
-
-        case '@': // end program
-          return;
-
-        case 'Y': // stack debug opcode
-          if (enable_debug_opcode) {
-            fprintf(stderr, "[stack debug: %zu items; item 0 at top]\n", stack.size());
-            for (size_t x = 0; x < stack.size(); x++) {
-              int64_t item = stack.at(x);
-              if (item >= 0x20 && item <= 0x7F) {
-                fprintf(stderr, "item %zu: %" PRId64 " (0x%" PRIX64 ") (%c)\n", x, item,
-                    item, static_cast<char>(item));
-              } else {
-                fprintf(stderr, "item %zu: %" PRId64 " (0x%" PRIX64 ")\n", x, item, item);
-              }
-            }
+      case '\"': { // push an entire string
+        pos.move_forward();
+        for (;;) {
+          int16_t value = field.get(pos.x, pos.y, pos.z);
+          if (value == '\"') {
             break;
           }
 
-        default:
-          throw invalid_argument(string_printf(
-              "can\'t interpret character %02hX '%c' at (%zd, %zd)", opcode,
-              opcode, pos.x, pos.y));
+          stack.push(value);
+          pos.move_forward();
+        }
+
+        // pos now points to the terminal quote; we'll automatically move
+        // forward after this loop
+        break;
+      }
+
+      case ';': { // skip over a segment
+        pos.move_forward();
+        for (;;) {
+          int16_t value = field.get(pos.x, pos.y, pos.z);
+          if (value == ';') {
+            break;
+          }
+          pos.move_forward();
+        }
+
+        // pos now points to the terminal semicolon; we'll automatically move
+        // forward after this loop
+        break;
+      }
+
+      case ':': { // duplicate top of stack
+        int64_t i = stack.pop();
+        stack.push(i);
+        stack.push(i);
+        break;
+      }
+
+      case '\\': { // swap top 2 items on stack
+        int64_t a = stack.pop();
+        int64_t b = stack.pop();
+        stack.push(a);
+        stack.push(b);
+        break;
+      }
+
+      case '$': // discard top of stack
+        stack.pop();
+        break;
+
+      case '.': // pop and print as integer followed by space
+        printf("%" PRId64 " ", stack.pop());
+        break;
+
+      case ',': // pop and print as ascii character
+        putchar(stack.pop());
+        break;
+
+      case ' ': // skip this cell
+        break;
+
+      case '#': // skip this cell and next cell
+        pos.move_forward();
+        break;
+
+      case 'p': { // write program space
+        int64_t y = stack.pop();
+        int64_t x = stack.pop();
+        int64_t v = stack.pop();
+        field.set(x, y, 0, v);
+        break;
+      }
+
+      case 'g': // read program space
+        try {
+          int64_t y = stack.pop();
+          int64_t x = stack.pop();
+          stack.push(field.get(x, y, 0));
+        } catch (const out_of_range&) {
+          stack.push(' ');
+        }
+        break;
+
+      case '&': { // push user-supplied number
+        int64_t i;
+        scanf("%" PRId64, &i);
+        stack.push(i);
+        break;
+      }
+
+      case '~': // push user-supplied character
+        stack.push(getchar());
+        break;
+
+      case '@': // end program
+        return;
+
+      case 'Y': // stack debug opcode
+        if (enable_debug_opcode) {
+          fprintf(stderr, "[stack debug: %zu items; item 0 at top]\n", stack.size());
+          for (size_t x = 0; x < stack.size(); x++) {
+            int64_t item = stack.at(x);
+            if (item >= 0x20 && item <= 0x7F) {
+              fprintf(stderr, "item %zu: %" PRId64 " (0x%" PRIX64 ") (%c)\n", x, item,
+                  item, static_cast<char>(item));
+            } else {
+              fprintf(stderr, "item %zu: %" PRId64 " (0x%" PRIX64 ")\n", x, item, item);
+            }
+          }
+          break;
+        }
+
+      default:
+        throw invalid_argument(string_printf(
+            "can\'t interpret character %02hX '%c' at (%zd, %zd)", opcode,
+            opcode, pos.x, pos.y));
     }
     pos.move_forward();
   }
--- Main.cc
+++ Main.cc
@@ -22,14 +22,15 @@ using namespace std;
 
 
 
-enum Behavior {
+enum class Behavior {
   Interpret = 0,
   Compile = 1,
   Execute = 2,
 };
 
-enum Language {
-  Brainfuck = 0,
+enum class Language {
+  Automatic = 0,
+  Brainfuck,
   Befunge,
   Malbolge,
 };
@@ -38,16 +39,17 @@ enum Language {
 
 int main(int argc, char* argv[]) {
 
-  Language language;
+  Language language = Language::Automatic;
   int optimize_level = 1;
   uint8_t dimensions = 2;
   size_t memory_size = 0x100000; // 1MB
   size_t expansion_size = 0x10000; // 64KB
   size_t num_bad_options = 0;
+  bool verbose = false;
   bool assembly = false;
   bool intel_syntax = false;
   bool enable_debug_opcode = false;
-  Behavior behavior = Behavior::Interpret;
+  Behavior behavior = Behavior::Execute;
 
   int x;
   const char* input_filename = NULL;
@@ -55,19 +57,27 @@ int main(int argc, char* argv[]) {
   for (x = 1; x < argc; x++) {
 
     // all-language options
-    if (!strcmp(argv[x], "--assembly")) {
+    if (!strcmp(argv[x], "--verbose")) {
+      verbose = true;
+    } else if (!strcmp(argv[x], "--assembly")) {
       assembly = true;
     } else if (!strcmp(argv[x], "--intel-syntax")) {
       intel_syntax = true;
 
     // mode selection
+    } else if (!strcmp(argv[x], "--interpret")) {
+      behavior = Behavior::Interpret;
     } else if (!strcmp(argv[x], "--execute")) {
       behavior = Behavior::Execute;
+    } else if (!strcmp(argv[x], "--compile")) {
+      behavior = Behavior::Compile;
     } else if (!strncmp(argv[x], "--output-filename=", 18)) {
       behavior = Behavior::Compile;
       output_filename = &argv[x][18];
 
     // language selection
+    } else if (!strcmp(argv[x], "--automatic")) {
+      language = Language::Automatic;
     } else if (!strcmp(argv[x], "--brainfuck")) {
       language = Language::Brainfuck;
     } else if (!strcmp(argv[x], "--befunge")) {
@@ -96,25 +106,45 @@ int main(int argc, char* argv[]) {
   }
 
   if (!input_filename) {
-    fprintf(stderr, "no input file\n");
+    fprintf(stderr, "equinox: no input file\n");
     num_bad_options++;
   }
 
   if (num_bad_options) {
-    fprintf(stderr, "\
+    fprintf(stderr, "\n\
 To interpret code: %s [options] input_file\n\
 To compile code: %s [options] input_file --output-filename=output_file\n\
 To execute code: %s -x [options] input_file\n\
 \n\
+Languages:\n\
+  --automatic\n\
+      Automatically detect the language based on the filename (default).\n\
+  --befunge\n\
+      Run the file as Funge-98.\n\
+  --brainfuck\n\
+      Run the file as Brainfuck.\n\
+  --malbolge\n\
+      Run the file as Malbolge.\n\
+\n\
+Modes:\n\
+  --interpret\n\
+      Run the code under an interpreter.\n\
+  --execute\n\
+      Compile the code and run it immediately (default).\n\
+  --compile\n\
+      Compile the code to an executable. Implied if --output-filename is used.\n\
+\n\
 Options for all languages:\n\
   --assembly\n\
-      In compile mode, output amd64 assembly code instead of an executable. In\n\
-      execute mode, output the compiled code\'s disassembly before running it.\n\
+      In compile mode, output amd64 assembly code instead of an executable.\n\
+      In execute mode, output the compiled code\'s disassembly before running.\n\
+      No effect in interpret mode (no assembly is produced).\n\
   --intel-syntax\n\
       In compile mode, output code using Intel syntax instead of AT&T syntax.\n\
       No effect in execute mode (debugging output is always Intel syntax).\n\
+      No effect in interpret mode (no assembly is produced).\n\
 \n\
-Brainfuck can run in all modes. Options:\n\
+Brainfuck runs in all modes. Language-specific options:\n\
   --memory-size=num_bytes\n\
       In compile mode, sets the overall memory size for the compiled program\n\
       (default 1048576). In interpret and execute mode, sets the size by which\n\
@@ -126,12 +156,29 @@ Brainfuck can run in all modes. Options:\n\
       Level 1: Collapse repeated opcodes into more efficient instructions.\n\
       Level 2: Eliminate memory boundary checks.\n\
 \n\
-Befunge runs only in execute or interpret mode. Options:\n\
+Befunge runs only in execute or interpret mode. Language-specific options:\n\
   --dimensions=num\n\
       Chooses the sublanguage to use. 1 for Unefunge, 2 for Befunge (default),\n\
       3 for Trefunge.\n\
+\n\
+Malbolge runs only in interpret mode. There are no language-specific options.\n\
 ", argv[0], argv[0], argv[0]);
     return 1;
+  }
+
+  if (language == Language::Automatic) {
+    if (ends_with(input_filename, ".b")) {
+      language = Language::Brainfuck;
+    } else if (ends_with(input_filename, ".bf")) {
+      language = Language::Befunge;
+    } else if (ends_with(input_filename, ".b98")) {
+      language = Language::Befunge;
+    } else if (ends_with(input_filename, ".mal")) {
+      language = Language::Malbolge;
+    } else {
+      fprintf(stderr, "equinox: cannot detect language\n");
+      return 1;
+    }
   }
 
   try {
